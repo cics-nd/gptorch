Fix squared_distance to get correct second derivative flow with detach()

gptorch/util.py
@@ -128,10 +128,15 @@ def squared_distance(x1: TensorType, x2: TensorType=None) -> TensorType:
     """
     Given points x1 [n1 x d1] and x2 [n2 x d2], return a [n1 x n2] matrix with
     the pairwise squared distances between the points.
+
+    Entry (i, j) is sum_{j=1}^d (x_1[i, j] - x_2[i, j]) ^ 2
     """
     if x2 is None:
         return squared_distance(x1, x1)
     x1s = x1.pow(2).sum(1, keepdim=True)
     x2s = x2.pow(2).sum(1, keepdim=True)
-    # Prevent negative squared distances
-    return torch.clamp(x1s + x2s.t() -2.0 * x1 @ x2.t(), min=0.0)
+    r2 = x1s + x2s.t() -2.0 * x1 @ x2.t()
+    # Prevent negative squared distances using torch.clamp
+    # NOTE: Clamping is for numerics.
+    # This use of .detach() is to avoid breaking the gradient flow.
+    return r2 - (torch.clamp(r2, max=0.0)).detach()