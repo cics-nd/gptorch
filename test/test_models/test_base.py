a076abd955c9e088106ba3df444c7361660b3d4f
Steven Atkinson <steven@atkinson.mn>
2019-10-23 20:58:03 -0400

    Placeholders for more tests

--- test/test_models/test_base.py
+++ test/test_models/test_base.py
@@ -20,6 +20,12 @@ class TestGPModel(object):
     Tests for the GPModel class
     """
 
+    def test_predict_f(self):
+        raise NotImplementedError()
+
+    def test_predict_y(self):
+        raise NotImplementedError()
+
     def test_predict_f_samples(self):
         # TODO mock a GPModel?  Using GPR for the moment.
         n, dx, dy = 5, 3, 2
