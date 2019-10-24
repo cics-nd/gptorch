73054e23b2da2805849c05cde424b4e7261fc6ef
Steven Atkinson <steven@atkinson.mn>
2019-10-23 21:13:58 -0400

    Refactor unit tests on base model, add predict_f, predict_y

--- test/test_models/test_base.py
+++ test/test_models/test_base.py
@@ -8,11 +8,9 @@ import numpy as np
 
 from gptorch.models.base import GPModel
 from gptorch.mean_functions import Zero
-from gptorch.util import torch_dtype
+from gptorch.util import TensorType
 from gptorch.kernels import Rbf
 from gptorch.models import GPR
-
-torch.set_default_dtype(torch_dtype)
 
 
 class TestGPModel(object):
@@ -21,33 +19,22 @@ class TestGPModel(object):
     """
 
     def test_predict_f(self):
-        raise NotImplementedError()
+        self._predict_fy("predict_f")
 
     def test_predict_y(self):
-        raise NotImplementedError()
+        self._predict_fy("predict_y")
 
     def test_predict_f_samples(self):
-        # TODO mock a GPModel?  Using GPR for the moment.
-        n, dx, dy = 5, 3, 2
-        x, y = np.random.randn(n, dx), np.random.randn(n, dy)
-        kern = Rbf(dx, ARD=True)
-        gp = GPR(x, y, kern)
-
-        n_test = 5
-        x_test = np.random.randn(n_test, dx)
-        f_samples = gp.predict_f_samples(x_test)
-        assert isinstance(f_samples, torch.Tensor)
-        assert f_samples.ndimension() == 3  # [sample x n_test x dy]
-        assert f_samples.shape == (1, n_test, dy)
-
-        n_samples = 3
-        f_samples_2 = gp.predict_f_samples(x_test, n_samples=n_samples)
-        assert isinstance(f_samples_2, torch.Tensor)
-        assert f_samples_2.ndimension() == 3  # [sample x n_test x dy]
-        assert f_samples_2.shape == (n_samples, n_test, dy)
+        self._predict_fy_samples("predict_f_samples")
 
     def test_predict_y_samples(self):
-        # TODO mock a GPModel?  Using GPR for the moment.
+        self._predict_fy_samples("predict_y_samples")
+
+    def _predict_fy(self, attr):
+        """
+        attr='predict_f' or 'predict_y'
+        """
+
         n, dx, dy = 5, 3, 2
         x, y = np.random.randn(n, dx), np.random.randn(n, dy)
         kern = Rbf(dx, ARD=True)
@@ -55,13 +42,47 @@ class TestGPModel(object):
 
         n_test = 5
         x_test = np.random.randn(n_test, dx)
-        y_samples = gp.predict_y_samples(x_test)
-        assert isinstance(y_samples, torch.Tensor)
-        assert y_samples.ndimension() == 3  # [sample x n_test x dy]
-        assert y_samples.shape == (1, n_test, dy)
+        f = getattr(gp, attr)
+        mu, v = f(x_test)
+        for result in [mu, v]:
+            assert isinstance(result, np.ndarray)
+            assert result.ndim == 2  # [n_test x dy]
+            assert result.shape == (n_test, dy)
+
+        x_test_torch = TensorType(x_test)
+        mu_torch, v_torch = f(x_test_torch)
+        for result in [mu_torch, v_torch]:
+            assert isinstance(result, TensorType)
+            assert result.ndimension() == 2  # [n_test x dy]
+            assert result.shape == (n_test, dy)
+
+    def _predict_fy_samples(self, attr):
+        """
+        attr="predict_f_samples" or "predict_y_samples"
+        """
+
+        # TODO mock a GPModel?  Using GPR for the moment.
+        n, dx, dy = 5, 3, 2
+        x, y = np.random.randn(n, dx), np.random.randn(n, dy)
+        kern = Rbf(dx, ARD=True)
+        gp = GPR(x, y, kern)
+        f = getattr(gp, attr)
+
+        n_test = 5
+        x_test = np.random.randn(n_test, dx)
+        samples = f(x_test)
+        assert isinstance(samples, np.ndarray)
+        assert samples.ndim == 3  # [sample x n_test x dy]
+        assert samples.shape == (1, n_test, dy)
 
         n_samples = 3
-        y_samples_2 = gp.predict_y_samples(x_test, n_samples=n_samples)
-        assert isinstance(y_samples_2, torch.Tensor)
-        assert y_samples_2.ndimension() == 3  # [sample x n_test x dy]
-        assert y_samples_2.shape == (n_samples, n_test, dy)
+        samples_2 = f(x_test, n_samples=n_samples)
+        assert isinstance(samples_2, np.ndarray)
+        assert samples_2.ndim == 3  # [sample x n_test x dy]
+        assert samples_2.shape == (n_samples, n_test, dy)
+
+        x_test_torch = TensorType(x_test)
+        samples_torch = f(x_test_torch)
+        assert isinstance(samples_torch, TensorType)
+        assert samples_torch.ndimension() == 3  # [1 x n_test x dy]
+        assert samples_torch.shape == (1, n_test, dy)
