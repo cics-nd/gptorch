c82d2e0afd1d432def9f5d8654d672afeb124d34
Steven Atkinson <steven@atkinson.mn>
2021-10-30 08:28:52 -0700

    Fix bug with pip-installed torch w/ nonstandard CUDA versions

---
+++ test/test_functions.py
@@ -0,0 +1,7 @@
+# File: test_functions.py
+# File Created: Saturday, 30th October 2021 11:06:35 am
+# Author: Steven Atkinson (steven@atkinson.mn)
+
+def test_importable():
+    from gptorch import functions
+    
